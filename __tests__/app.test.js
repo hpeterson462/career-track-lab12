d91350989fbea46702cf9c6e81cef3b7e38fac82
Heather Peterson <hpeterson462@gmail.com>
2020-09-28 14:46:21 -0700

    POST route and test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,23 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/user-service');
 
-describe('alchemy-app routes', () => {
+describe('user routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+  });
+
+  it('signs up a user via POST', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        id: expect.any(String),
+        email: 'h@h.com',
+      });
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'h@h.com'
+    });
   });
 });
